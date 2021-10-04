d5f8850b46c5eb737aa77663845a69dee255e5a4
Hasibul Hasan Hasib <mdhasibulctg100@gmail.com>
2021-10-04 13:58:11 +0600

    local_storage_fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Router>
       <Header></Header>
-      <Shop></Shop>
+        <Switch>
+          <Route exact path='/'>
+            <Shop/>
+          </Route>
+          <Route path='/shop'>
+            <Shop/>
+          </Route>
+          <Route path='/orders'>
+            <OrderReview/>
+          </Route>
+          <Route path='/inventory'>
+            <Inventory/>
+          </Route>
+          <Route path='*'>
+            <NotFound/>
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import useProducts from '../Hooks/Hooks';
+import useCart from '../Hooks/useCart';
 
 const OrderReview = () => {
+    const [products] = useProducts();
+    const [cart] = useCart(products);
     return (
         <div>
-            <h2>This is Order Review</h2>
+            <h1>{products.length}</h1>
+            <h2>{cart.length}</h2>
         </div>
     );
 };
